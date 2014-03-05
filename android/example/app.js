4ae054985644e235e81db91dc108defbfaefb48b
Adam Paxton <adampax@gmail.com>
2014-03-04 18:10:30 -0600

    initial android commit

---
+++ android/example/app.js
@@ -0,0 +1,17 @@
+var win = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+var label = Ti.UI.createLabel();
+win.add(label);
+win.open();
+
+// TODO: write your module tests here
+var amplitude = require('com.polancomedia.amplitude');
+
+
+//must be called first
+//amplitude.initialize('YOUR_API_KEY');
+
+amplitude.setUserId('androidUser');
+
+amplitude.logEvent('android_event');
